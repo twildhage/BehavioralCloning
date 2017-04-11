399a051a150f39d7daf1af7dea737d22a8d5daea
Unknown <wildhage.timo@gmail.com>
2017-04-12 00:56:07 +0200

    feat: Add image preprocessing and augmentation steps
    Add functions:
    - random_flip
    - cut
    - resize

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,16 @@
 import numpy as np
 import cv2
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
 from sklearn.utils import shuffle
 
 
-DATA_PATH = '/home/timo/Documents/mldata/car_sim_video_images/training_data/driving_log.csv'
+DATA_PATH = '/home/timo/Documents/mldata/car_sim_video_images/training_data/'
+DRIVING_LOG_FILE = 'driving_log.csv'
 
-def gamma_correction(image, mode='random', gamma=1.25):
+
+def random_gamma_shift(image, mode='random', gamma=1.25):
     """
     A gamma correction is used to change the brightness of training images.
     The correction factor 'gamma' is sampled randomly in order to generated
@@ -34,31 +37,50 @@ def gamma_correction(image, mode='random', gamma=1.25):
     # apply gamma correction using the lookup table
     return cv2.LUT(image, table)
 
-def get_consistent_modification_of(image, angle):
-    """
-    some description
-    :image
-        sss
-    :angle
-        sss
-    :return
-        XXX
-    """
+
+def random_flip(image, angle):
     flip_the_image = bernoulli.rvs(0.5)
     if flip_the_image == True:
         return np.fliplr(image), -angle
     else:
         return image, angle
 
+def cut(image, upper, lower):
+    height = image.shape[0]
+    l = int(height * upper)
+    u = int(height * (1-lower))
+    print(l, u)
+    return image[l:u,::]
 
-def get_random_subset_of_dataset(subset_size, datafile):
+def resize(image, height, width):
+    return cv2.resize(image, (height, width))
+
+
+def get_consistent_modification_of(image, angle):
+    """
+    some description
+    :image
+        image from one camera position (left, right or center)
+    :angle
+        steering angle
+    :return
+        modified image and angle
+    """
+    image, angle = random_flip(image, angle)
+    image        = random_gamma_shift(image)
+    image        = cut(image, 0.38, 0.15)
+    image        = resize(image, 48, 48)
+    return image, angle
+
+
+def get_random_subset_of_dataset(subset_size, filename):
     camera_names     = ('center', 'left', 'right')
     # Create dictionary: {0:'center', ...}
     cameras          = dict({key:val for key,val in enumerate(camera_names)})
     # Create dictionary: {'center':0.0, ...}
     steering_offsets = dict({key:val for key,val in zip(camera_names, [0.0, 0.2, -0.2])})
 
-    dataset          = pd.read_csv(datafile)
+    dataset          = pd.read_csv(filename)
     # Shuffle dataset, so that the first n entries represent a random subset
     dataset = shuffle(dataset)
     # Create a list with random cameras: ['left', 'left', 'center', 'right', ...]
@@ -69,7 +91,9 @@ def get_random_subset_of_dataset(subset_size, datafile):
     return img_files, angles
 
 
-def generate_batch(batch_size, img_path, filename):
+
+
+def generate_batch(batch_size, nb_batches, img_path, filename):
     """
     This function generates a generator, which then yields a training batch.
     If this sounds confusing, check out this excellent explanation on
@@ -77,12 +101,14 @@ def generate_batch(batch_size, img_path, filename):
     http://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do-in-python
     """
     cnt = 0
-    while cnt < 3:
+    while cnt < nb_batches:
         X_batch = []
         y_batch = []
         img_files, angles = get_random_subset_of_dataset(batch_size, (img_path + filename))
         for img_file, angle in zip(img_files, angles):
             img = plt.imread(img_path + img_file)
+            # Modify images
+            img, angle = get_consistent_modification_of(img, angle)
             X_batch.append(img)
             y_batch.append(angle)
         yield np.array(X_batch), np.array(y_batch)
